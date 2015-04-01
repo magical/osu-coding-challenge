7883ad08b1d41af5478edb55bfeee46632edc61d
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-03-31 18:42:17 -0700

    Euler 15

---
+++ euler15.py
@@ -0,0 +1,13 @@
+import functools
+
+@functools.lru_cache(maxsize=None)
+def paths(x, y):
+    if x < 0 or y < 0:
+        return 0
+    if x == 0 and y == 0:
+        return 1# 0?
+    return paths(x-1, y) + paths(x, y-1)
+        
+#print(paths(6, 6))
+print(paths(20, 20))
+# 137846528820
